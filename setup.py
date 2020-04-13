fb00f8db12b932f747dd7dce9dcec586ffeacf31
Donghoon Lee <hoondy@users.noreply.github.com>
2020-04-13 12:47:21 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 from setuptools import setup
 
 setup(name='starrpeaker',
-      version='0.1',
-      description='STARR-seq peak caller',
-      keywords = "STARR-seq, peaks, bioinformatics",
-      url='http://github.com/hoondy/starrpeaker',
+      version='1.0',
+      description='STARRPeaker: STARR-seq peak caller',
+      keywords = "STARRPeaker, STARR-seq, peaks, bioinformatics",
+      url='http://github.com/gersteinlab/starrpeaker',
       author='Donghoon Lee',
       author_email='donghoon.lee@yale.edu',
       license='GPL',
       packages=['starrpeaker'],
       install_requires=[
-          'numpy',
-          'scipy',
-          'pandas',
-          'statsmodels',
-          'pysam',
-          'pybedtools',
-          'pyBigWig'
+            'numpy',
+            'scipy',
+            'pandas',
+            'statsmodels<=0.10.2',
+            'pysam',
+            'pybedtools',
+            'pyBigWig',
+            'scikit-learn'
       ],
       entry_points = {
           'console_scripts': [
-              'starrpeaker = starrpeaker.starrpeaker.starrpeaker:main'
+              'starrpeaker = starrpeaker.starrpeaker:main'
           ]
           }
       )
